bf1c31100cd1529a4d48ec906ac49efd88db02e2
dcortesnet <dcortes.net@gmail.com>
2023-06-27 10:06:55 -0400

    Validation OS in child process

--- 9. child process/child-process-declatarion.js
+++ 9. child process/child-process-declatarion.js
@@ -1,5 +1,14 @@
 const { exec } = require('child_process');
+const os = require('os');
 
-url = "https://www.youtube.com/";
-cmdCommand = `start chrome /new-tab ${url}`;
+const platform = os.platform()
+const url = "https://www.youtube.com/";
+let cmdCommand;
+
+if (platform === "win32") {
+  cmdCommand = `start chrome /new-tab ${url}`;
+} else {
+  cmdCommand = `open -na 'Google Chrome' --args --new-window ${url}`;
+}
+
 exec(cmdCommand);
